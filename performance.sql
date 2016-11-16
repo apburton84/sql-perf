8c3b48ac80f1044e8851813230b2dfc9b5b78005
anthony burton <ant@anthonys-MacBook-Pro.local>
2016-11-16 22:29:46 +0000

    Initial Commit

---
+++ performance.sql
@@ -0,0 +1,108 @@
+-- Get INNODB Status
+SHOW ENGINE INNODB STATUS;
+
+SHOW GLOBAL STATUS LIKE "Innodb%"; -- See all
+
+SHOW GLOBAL STATUS LIKE 'Innodb_buffer_pool_pages_data';  -- IBPDataPages 
+SHOW GLOBAL STATUS LIKE 'Innodb_buffer_pool_pages_total'; -- IBPTotalPages
+SHOW GLOBAL STATUS LIKE 'Innodb_page_size'; 			  -- IPS
+
+-- Is the performance scheme turned on
+SHOW VARIABLES LIKE 'performance_schema';
+
+-- SET GLOBAL userstat=1;
+
+-- FLUSH TABLE_STATISTICS  -- MariaDB 5.2
+-- FLUSH INDEX_STATISTICS  -- MariaDB 5.2
+-- FLUSH USER_STATISTICS   -- MariaDB 5.2
+-- FLUSH CLIENT_STATISTICS -- MariaDB 5.2
+
+SHOW CLIENT_STATISTICS -- MariaDB 5.2
+SHOW USER_STATISTICS   -- MariaDB 5.2
+SHOW INDEX_STATISTICS  -- MariaDB 5.2
+SHOW TABLE_STATISTICS  -- MariaDB 5.2
+
+-- Are slow queries turned on?
+SHOW GLOBAL VARIABLES LIKE '%SLOW_QUERY%'
+;
+-- SET GLOBAL SLOW_QUERY_LOG=1;
+
+SHOW GLOBAL VARIABLES LIKE '%LONG_QUERY%'
+;
+-- SET GLOBAL LONG_QUERY_TIME=1;
+
+-- innodb_buffer_pool
+        
+    -- TO see the Buffer Pool size IN GB run this:
+	SELECT FORMAT(BufferPoolPages*PageSize/POWER(1024,3),2) BufferPoolDataGB FROM
+	(SELECT variable_value BufferPoolPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_total') A,
+	(SELECT variable_value PageSize FROM information_schema.global_status	
+	 WHERE variable_name = 'Innodb_page_size') B
+	 ;
+    
+    -- If larger than 95%, set innodb_buffer_pool_size to 75% of the DB Server's RAM. 
+    SET @IBPDataPages  = (SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'Innodb_buffer_pool_pages_data');  -- SELECT @IBPDataPages;
+    SET @IBPTotalPages = (SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'Innodb_buffer_pool_pages_total'); -- SELECT @IBPTotalPages;
+    SET @IBPPctFull    = CAST(@IBPDataPages * 100.0 / @IBPTotalPages AS DECIMAL(5,2));
+    SELECT @IBPPctFull;
+    
+    -- If the above returns less than 95%, then the following number for IBPSize (in GB) fits more closely to your actual working dataset.
+    SET @IBPSize 	  = (SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'Innodb_page_size'); 			 	-- SELECT @IBPSize;
+    SET @IBPDataPages = (SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'Innodb_buffer_pool_pages_data'); 	-- SELECT @IBPDataPages;
+    SET @IBPSize 	  = concat(ROUND(@IBPSize * @IBPDataPages / (1024*1024*1024) * 1.05, 2), ' GB' );
+    SELECT @IBPSize;
+
+    -- Recommended InnoDB Buffer Pool Size based on all InnoDB Data and Indexes with an additional 10%
+    SELECT CONCAT(CEILING(RIBPS/POWER(1024,pw)),SUBSTR(' KMGT',pw+1,1))
+    Recommended_InnoDB_Buffer_Pool_Size FROM
+    (
+        SELECT RIBPS,FLOOR(LOG(RIBPS)/LOG(1024)) pw
+        FROM
+        (
+            SELECT SUM(data_length+index_length)*1.1*growth RIBPS
+            FROM information_schema.tables AAA,
+            (SELECT 1.25 growth) BBB
+            WHERE ENGINE='InnoDB'
+        ) AA
+    ) A; 
+     
+    -- Actual GB of MEMORY IS IN USE BY INNODB DATA
+    SELECT (
+        PagesData*PageSize)/POWER(1024,3) DataGB FROM
+            (SELECT variable_value PagesData
+             FROM information_schema.global_status
+             WHERE variable_name='Innodb_buffer_pool_pages_data') A,
+            (SELECT variable_value PageSize
+             FROM information_schema.global_status
+             WHERE variable_name='Innodb_page_size') B
+    ;  
+    
+    -- TO see the amount of DATA IN the Buffer Pool size IN GB run this:
+	SELECT FORMAT(BufferPoolPages*PageSize/POWER(1024,3),2) BufferPoolDataGB FROM
+	(SELECT variable_value BufferPoolPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_data') A,
+	(SELECT variable_value PageSize FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_page_size') B;  
+       
+    -- TO see the percentage of the Buffer Pool IN USE, run this:
+	SELECT CONCAT(FORMAT(DataPages*100.0/TotalPages,2),' %') BufferPoolDataPercentage FROM
+	(SELECT variable_value DataPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_data') A,
+	(SELECT variable_value TotalPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_total') B;
+    
+    -- TO see the Space Taken Up BY Dirty Pages, run this:
+	SELECT FORMAT(DirtyPages*PageSize/POWER(1024,3),2) BufferPoolDirtyGB FROM
+	(SELECT variable_value DirtyPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_dirty') A,
+	(SELECT variable_value PageSize FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_page_size') B;
+	
+	-- To see the Percentage of Dirty Pages, run this:
+	SELECT CONCAT(FORMAT(DirtyPages*100.0/TotalPages,2),' %') BufferPoolDirtyPercentage FROM
+	(SELECT variable_value DirtyPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_dirty') A,
+	(SELECT variable_value TotalPages FROM information_schema.global_status
+	 WHERE variable_name = 'Innodb_buffer_pool_pages_total') B;
+    
